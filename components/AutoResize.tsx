0f7d931b345ce84f2883f0bd7620bcffe55f33c6
Rudi <rudi@MacBook-Air.local>
2025-10-23 19:26:22 +0200

    changes

--- components/AutoResize.tsx
+++ components/AutoResize.tsx
@@ -4,8 +4,15 @@ import { useEffect } from 'react';
 export default function AutoResize() {
   useEffect(() => {
     const parentOrigin = process.env.NEXT_PUBLIC_PARENT_ORIGIN || '*';
-    const post = () => window.parent.postMessage({ type: 'agent:height', height: document.documentElement.scrollHeight }, parentOrigin);
-    const ro = new ResizeObserver(() => post());
+    const post = () => {
+      const height = document.documentElement.scrollHeight;
+      console.log('AutoResize: Sending height message:', height);
+      window.parent.postMessage({ type: 'agent:height', height }, parentOrigin);
+    };
+    const ro = new ResizeObserver(() => {
+      console.log('AutoResize: ResizeObserver triggered');
+      post();
+    });
     ro.observe(document.documentElement);
     post();
     return () => ro.disconnect();
